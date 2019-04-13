4a2582bb7760431beff9f75f45d1e87c0a48f3db
Stuart Shay <sshay@yahoo.com>
2019-04-12 21:18:10 -0400

    Update

---
+++ src/MicroService.Service/Services/CalculationService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using MicroService.Data.Repository;
+using MicroService.Service.Helpers;
+
+namespace MicroService.Service.Services
+{
+    public class CalculationService : ICalculationService
+    {
+        private readonly ITestDataRepository _testDataRepository;
+
+        public CalculationService(ITestDataRepository testDataRepository)
+        {
+            _testDataRepository = testDataRepository ?? throw new ArgumentNullException(nameof(testDataRepository));
+        }
+
+        public async Task<double> CalculatePercentile(double[] sequence, double excelPercentile)
+        {
+            var data = await _testDataRepository.FindAll();
+            var array = data.Select(x => x.Data).ToArray();
+
+            return FunctionHelper.Percentile1(array, excelPercentile);
+        }
+    }
+}
