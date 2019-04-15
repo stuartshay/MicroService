b218b70705b7996986000c7d979a67aedeb2b9ee
Stuart Shay <sshay@yahoo.com>
2019-04-14 23:59:00 -0400

    Update Readme

--- docker/database-postgres.dockerfile/docker-entrypoint-initdb.d/002-int-dml.sh
+++ docker/database-postgres.dockerfile/docker-entrypoint-initdb.d/002-int-dml.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -ex
 
-psql -U postgres --command "CREATE TABLE public.test_data (	id serial NOT NULL, 'data' float8 NOT NULL, CONSTRAINT test_data_pkey PRIMARY KEY (id);"
-psql -U postgres --command "CREATE OR REPLACE FUNCTION public.get_data() RETURNS SETOF double precision LANGUAGE plpgsql STABLE AS $function$ BEGIN RETURN QUERY SELECT data FROM public.test_data; END $function$;"
+PGUSER=postgres psql <<- EOSQL
+ CREATE TABLE public.test_data (
+	id serial NOT NULL,
+	"data" float8 NOT NULL,
+	CONSTRAINT test_data_pkey PRIMARY KEY (id)
+);
+EOSQL
+
 
--- docker/database-postgres.dockerfile/docker-entrypoint-initdb.d/003-import-data.sh
+++ docker/database-postgres.dockerfile/docker-entrypoint-initdb.d/003-import-data.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
 set -ex
+
+PGUSER=postgres psql <<- EOSQL
+   COPY public.test_data FROM '/data/test_data_201904121704.csv' DELIMITER ',' CSV HEADER;
+EOSQL
