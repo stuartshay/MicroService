8772436ac246fdd45a2e51ef235619e513356b05
Stuart Shay <sshay@yahoo.com>
2020-09-09 00:28:25 -0400

    Update Tests

---
+++ src/MicroService.Service/Services/BoroughBoundariesService.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using MicroService.Service.Configuration;
+using MicroService.Service.Models;
+using NetTopologySuite.Features;
+using Microsoft.Extensions.Options;
+using NetTopologySuite.Geometries;
+using NetTopologySuite.IO;
+
+namespace MicroService.Service.Services
+{
+    public class BoroughBoundariesService : IBoroughBoundariesService
+    {
+        private readonly ShapefileDataReader _shapeFileDataReader;
+
+        public BoroughBoundariesService(IOptions<ApplicationOptions> options)
+        {
+            var shapeDirectory = $"{options.Value.ShapeConfiguration.ShapeRootDirectory}\\Borough_Boundaries\\nybb";
+            string shapePath = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), shapeDirectory));
+
+            GeometryFactory factory = new GeometryFactory();
+            _shapeFileDataReader = new ShapefileDataReader(shapePath, factory);
+
+        }
+
+        public List<Feature> GetFeatures()
+        {
+            var features = new List<Feature>();
+            while (_shapeFileDataReader.Read())
+            {
+                Feature feature = new Feature();
+                AttributesTable attributesTable = new AttributesTable();
+                DbaseFileHeader header = _shapeFileDataReader.DbaseHeader;
+
+                string[] keys = new string[header.NumFields];
+                var geometry = _shapeFileDataReader.Geometry;
+
+                for (int i = 0; i < header.NumFields; i++)
+                {
+                    DbaseFieldDescriptor fldDescriptor = header.Fields[i];
+                    keys[i] = fldDescriptor.Name;
+
+                    // First Field Geometry
+                    var value = _shapeFileDataReader.GetValue(i + 1);
+                    attributesTable.Add(fldDescriptor.Name, value);
+                }
+
+                feature.Geometry = geometry;
+                feature.Attributes = attributesTable;
+                features.Add(feature);
+            }
+
+            return features;
+        }
+
+        public BoroughBoundaries GetFeatureLookup(double x, double y)
+        {
+            var point = new Point(x, y);
+
+            var model = new BoroughBoundaries();
+            var features = GetFeatures();
+            foreach (var f in features)
+            {
+                var z = f.Geometry.Contains(point);
+                if (z)
+                {
+                    model = new BoroughBoundaries
+                    {
+                        BoroCode = f.Attributes["BoroCode"].ToString(),
+                        BoroName = f.Attributes["BoroName"].ToString(),
+                    };
+                }
+            }
+
+            return model;
+
+        }
+
+        public ShapefileHeader GetShapeProperties()
+        {
+            ShapefileHeader shpHeader = _shapeFileDataReader.ShapeHeader;
+            return shpHeader;
+        }
+
+        public DbaseFileHeader GetShapeDatabaseProperties()
+        {
+            DbaseFileHeader header = _shapeFileDataReader.DbaseHeader;
+            return header;
+        }
+    }
+}
---
+++ test/MicroService.Test/Unit/WktReaderFunctions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NetTopologySuite.Geometries;
+
+namespace MicroService.Test.Unit
+{
+    public static class WktReaderFunctions
+    {
+        public static void GeometryContainsPoint()
+        {
+            var reader = new NetTopologySuite.IO.WKTReader();
+            Geometry poly = reader.Read(
+                @"POLYGON ((428999.76819468878 360451.93329044303, 428998.25517286535 360420.80827007542,
+429023.1119599645 360406.75878171506, 429004.52340613387 360451.71714446822, 
+429004.52340613387 360451.71714446822, 428999.76819468878 360451.93329044303))");
+
+
+            var points = new List<NetTopologySuite.Geometries.Geometry>(new[]
+            {
+                reader.Factory.CreatePoint(new NetTopologySuite.Geometries.Coordinate(429012.5, 360443.18)),   
+                reader.Factory.CreatePoint(new NetTopologySuite.Geometries.Coordinate(429001.59, 360446.98)),
+                reader.Factory.CreatePoint(new NetTopologySuite.Geometries.Coordinate(429003.31, 360425.45)),
+                reader.Factory.CreatePoint(new NetTopologySuite.Geometries.Coordinate(429016.9, 360413.04))
+            });
+
+
+            var inside = new List<bool>(new[] { false, true, true, true });
+
+            for (var i = 0; i < points.Count; i++)
+            {
+                var contains = poly.Contains(points[i]);
+                System.Console.Write(poly.Contains(points[i]));
+            }
+        }
+
+        internal static void PolygonContainsPoints()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
