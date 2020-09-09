18f88bf9c9f5f811d750847536e05aaa0151f480
Stuart Shay <sshay@yahoo.com>
2020-09-09 00:28:25 -0400

    Update Tests

---
+++ src/MicroService.Service/Models/BoroughBoundaries.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroService.Service.Models
+{
+    public class BoroughBoundaries
+    {
+        public string BoroCode { get; set; }
+        public string BoroName { get; set; }
+    }
+}
---
+++ src/MicroService.Service/Services/IBoroughBoundariesService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MicroService.Service.Models;
+using NetTopologySuite.Features;
+using NetTopologySuite.Geometries;
+using NetTopologySuite.IO;
+
+namespace MicroService.Service.Services
+{
+    public interface IBoroughBoundariesService
+    {
+        List<Feature> GetFeatures();
+
+        BoroughBoundaries GetFeatureLookup(double x, double y);
+
+        ShapefileHeader GetShapeProperties();
+
+        DbaseFileHeader GetShapeDatabaseProperties();
+    }
+}
