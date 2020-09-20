0e0962ac793053ce3c7840b69138e1d3571b2e3e
Stuart Shay <sshay@yahoo.com>
2020-09-20 12:00:17 -0400

    Update .NET Build

---
+++ src/MicroService.WebApi/Extensions/Health/VersionHealthCheck.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Options;
+
+namespace MicroService.WebApi.Extensions.Health
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class VersionHealthCheck : IHealthCheck
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new CancellationToken())
+        {
+            var applicationVersionNumber = GetType().GetTypeInfo().Assembly.GetName().Version.ToString();
+
+            var result = Task.FromResult(string.IsNullOrEmpty(applicationVersionNumber) ? HealthCheckResult.Unhealthy("failed") : HealthCheckResult.Healthy(applicationVersionNumber));
+            return result;
+        }
+    }
+
+}
