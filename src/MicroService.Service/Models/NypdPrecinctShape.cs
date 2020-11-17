692a8cc37290bba98b9786b4a2be0f6ddd340f46
Stuart Shay <sshay@yahoo.com>
2020-11-16 21:12:52 -0500

    Update

---
+++ src/MicroService.Service/Models/NypdPrecinctShape.cs
@@ -0,0 +1,10 @@
+﻿using MicroService.Service.Models.Base;
+
+namespace MicroService.Service.Models
+{
+    public class NypdPrecinctShape : ShapeBase
+    {
+        public string Precinct { get; set; }
+
+    }
+}
