94e54f665326f0477aef7a0f3bb15a673e794b51
Stuart Shay <sshay@yahoo.com>
2020-12-16 19:50:16 -0500

    Update PSQL

---
+++ src/MicroService.WebApi/Extensions/MetricsRegistry.cs
@@ -0,0 +1,38 @@
+﻿using App.Metrics;
+using App.Metrics.Counter;
+
+namespace MicroService.WebApi.Extensions
+{
+    /// <summary>
+    /// App Metrics Registry
+    /// </summary>
+    public static class MetricsRegistry
+    {
+        /// <summary>
+        /// Get Shapes Counter
+        /// </summary>
+        public static CounterOptions GetShapesCounter => new CounterOptions
+        {
+            Name = "Shapes Counter",
+            MeasurementUnit = Unit.Calls,
+        };
+
+        /// <summary>
+        /// Get Shapes Properties Counter
+        /// </summary>
+        public static CounterOptions GetShapePropertiesCounter => new CounterOptions
+        {
+            Name = "Shapes Properties Counter",
+            MeasurementUnit = Unit.Calls,
+        };
+
+        /// <summary>
+        /// Get Feature Lookup Counter
+        /// </summary>
+        public static CounterOptions GetFeatureLookupCounter => new CounterOptions
+        {
+            Name = "Feature Lookup Counter",
+            MeasurementUnit = Unit.Calls,
+        };
+    }
+}
