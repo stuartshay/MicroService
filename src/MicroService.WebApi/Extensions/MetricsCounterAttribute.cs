e423df36cb7d705e1a76ff8de5e734e9659e129b
Stuart Shay <sshay@yahoo.com>
2020-12-21 21:19:33 -0500

    Update Core Settings

---
+++ src/MicroService.WebApi/Extensions/MetricsCounterAttribute.cs
@@ -0,0 +1,36 @@
+﻿using App.Metrics;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace MicroService.WebApi.Extensions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class MetricsCounterAttribute : IResultFilter
+    {
+        private readonly IMetrics _metrics;
+
+        public MetricsCounterAttribute(IMetrics metrics)
+        {
+            _metrics = metrics;
+        }
+
+
+        public void OnResultExecuting(ResultExecutingContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// Feature Lookup Counter
+        /// https://docs.microsoft.com/en-us/aspnet/core/mvc/controllers/filters?view=aspnetcore-5.0#action-filters
+        /// </summary>
+        /// <param name="context"></param>
+        public void OnResultExecuted(ResultExecutedContext context)
+        {
+            _metrics.Measure.Counter.Increment(MetricsRegistry.GetFeatureLookupCounter);
+        }
+
+
+    }
+}
