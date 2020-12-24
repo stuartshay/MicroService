af2b2603147dd43ffa03449ad41dc190a17580d2
Stuart Shay <sshay@yahoo.com>
2020-12-24 17:45:30 -0500

    Update

--- docker/postgres/docker-entrypoint-initdb.d/004-import-pluto.sh
+++ docker/postgres/docker-entrypoint-initdb.d/004-import-pluto.sh
@@ -2,11 +2,16 @@
 set -ex
 
 FILE=/data/nyc_pluto_20v7_csv.zip
+DESTINATION=/data/pluto
+
+mkdir -p $DESTINATION
+
 if [ ! -f "$FILE" ]; then
-    echo "File not found!"
+    curl -sS https://www1.nyc.gov/assets/planning/download/zip/data-maps/open-data/nyc_pluto_20v7_csv.zip > $FILE
+    unzip $FILE -d $DESTINATION
 fi
 
+PGUSER=postgres psql <<- EOSQL
+   COPY public.pluto FROM '/data/pluto/pluto_20v7.csv' DELIMITER ',' CSV HEADER;
+EOSQL
 
-
-#curl -sS https://www1.nyc.gov/assets/planning/download/zip/data-maps/open-data/nyc_pluto_20v7_csv.zip > /data/nyc_pluto_20v7_csv.zip
-#unzip /data/nyc_pluto_20v7_csv.zip
