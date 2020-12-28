02f55a914301f639efcb399e877d2c4fa85ac20d
Stuart Shay <sshay@yahoo.com>
2020-12-28 18:03:47 -0500

    Update

---
+++ src/MicroService.Service/Helpers/FileHelpers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace MicroService.Service.Helpers
+{
+    public static class FileHelpers
+    {
+        public static string GetFilesDirectory()
+        {
+            var appPath = GetAssemblyDirectory();
+            var dataPath = @"../../../../../files";
+            var dataDirectory = Path.GetFullPath(Path.Combine(appPath, dataPath));
+
+            return dataDirectory;
+        }
+
+        private static string GetAssemblyDirectory()
+        {
+            string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+            UriBuilder uri = new UriBuilder(codeBase);
+            string path = Uri.UnescapeDataString(uri.Path);
+            return Path.GetDirectoryName(path);
+        }
+
+    }
+}
---
+++ src/MicroService.Service/Models/Enum/FlatFileProperties.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace MicroService.Service.Models.Enum
+{
+    public enum FlatFileProperties
+    {
+        [Description("Subway Station Locations")]
+        [FlatFileAttributes("Subway_Station_Locations", "Stations.csv", FileTypes.Csv)]
+        SubwayStationLocations,
+
+        [Description("Subway Station Complex")]
+        [FlatFileAttributes("Subway_Complex", "StationComplexes.csv", FileTypes.Csv)]
+        SubwayStationComplex,
+
+    }
+}
