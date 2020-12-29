1f6edb281338019fdc36eb31555f2a3845154e86
Stuart Shay <sshay@yahoo.com>
2020-12-28 22:20:43 -0500

    Update

--- src/MicroService.Service/Models/DataMaps/StationComplexDataMap.cs
+++ src/MicroService.Service/Models/DataMaps/StationComplexDataMap.cs
@@ -3,14 +3,12 @@ using MicroService.Service.Models.FlatFileModels;
 
 namespace MicroService.Service.Models.DataMaps
 {
-    public class StationDataMap : ClassMap<StationFlatFile>
+    public sealed class StationComplexDataMap : ClassMap<StationComplexFlatFile>
     {
-        public StationDataMap()
+        public StationComplexDataMap()
         {
-            Map(m => m.StationId).Name("Station ID");
             Map(m => m.ComplexId).Name("Complex ID");
-            Map(m => m.GtfsStopId).Name("GTFS Stop ID");
-
+            Map(m => m.ComplexName).Name("Complex Name");
         }
         
     }
--- src/MicroService.Service/Models/FlatFileModels/StationFlatFile.cs
+++ src/MicroService.Service/Models/FlatFileModels/StationFlatFile.cs
@@ -4,10 +4,35 @@ namespace MicroService.Service.Models.FlatFileModels
 {
     public class StationFlatFile : FlatFileBase
     {
-        public string StationId { get; set; }
+        public int StationId { get; set; }
 
-        public string ComplexId { get; set; }
+        public int ComplexId { get; set; }
 
         public string GtfsStopId { get; set; }
+
+        public string Division { get; set; }
+
+        public string Line { get; set; }
+
+        public string StopName { get; set; }
+
+        public string Borough { get; set; }
+
+        public string DaytimeRoutes { get; set; }
+
+        public string Structure { get; set; }
+
+        public double GTFSLatitude { get; set; }
+        
+        public double GTFSLongitude { get; set; }
+
+        public string NorthDirectionLabel { get; set; }
+
+        public string SouthDirectionLabel { get; set; }
+
+        public bool ADA { get; set; }
+        
+        public string ADANotes { get; set; }
+
     }
 }
