ac02f30ccc94f071ce915e99050f6ae297881457
Stuart Shay <sshay@yahoo.com>
2021-04-03 13:44:44 -0400

    Add Serilog

---
+++ MicroService.Common/Logging/Logging.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using Serilog.Exceptions;
+
+
+namespace MicroService.Common.Logging
+{
+    public static class Logging
+    {
+        public static Action<HostBuilderContext, LoggerConfiguration> ConfigureLogger =>
+           (hostingContext, loggerConfiguration) =>
+           {
+               var env = hostingContext.HostingEnvironment;
+
+               loggerConfiguration.MinimumLevel.Information()
+                   .Enrich.FromLogContext()
+                   .Enrich.WithExceptionDetails()
+                   .Enrich.WithMachineName()
+                   .Enrich.WithProperty("Version", Assembly.GetEntryAssembly()?.GetName().Version)
+                   .Enrich.WithProperty("ApplicationName", env.ApplicationName)
+                   .Enrich.WithProperty("EnvironmentName", env.EnvironmentName)
+                   .Enrich.WithExceptionDetails()
+                   .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                   .MinimumLevel.Override("System.Net.Http.HttpClient", LogEventLevel.Warning)
+                   .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Information)
+                   .WriteTo.Console();
+           };
+
+    }
+}
