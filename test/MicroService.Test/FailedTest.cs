0a2975f0f41a6845944a79cc7d7a6202dca2eb65
Stuart Shay <sshay@yahoo.com>
2021-10-25 02:07:53 -0400

    Update Code

---
+++ test/MicroService.Test/FailedTest.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace MicroService.Test
+{
+    public class FailedTest
+    {
+        [Fact(DisplayName = "Failed_Test - Unit")]
+        [Trait("Category", "Unit")]
+        public void Failed_Test_Remove()
+        {
+            // Assert
+            Assert.False(true);
+        }
+    }
+}
