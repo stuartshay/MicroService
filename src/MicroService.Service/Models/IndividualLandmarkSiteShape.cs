ffc6ebf0d1988f025d3e158c6f2bce81a3c2e5d9
Stuart Shay <sshay@yahoo.com>
2023-02-23 20:44:29 -0500

    Update Build

---
+++ src/MicroService.Service/Models/IndividualLandmarkSiteShape.cs
@@ -0,0 +1,11 @@
+﻿using MicroService.Service.Models.Base;
+
+namespace MicroService.Service.Models
+{
+    public class IndividualLandmarkSiteShape : ShapeBase
+    {
+        public int BoroCode { get; set; }
+
+        public string BoroName { get; set; }
+    }
+}
