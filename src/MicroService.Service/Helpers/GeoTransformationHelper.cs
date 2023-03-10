7cbe2f03ccda58218e2f8b02ebce0968d57ab3dc
Stuart Shay <sshay@yahoo.com>
2023-03-10 17:22:16 -0500

    Update

--- src/MicroService.Service/Helpers/GeoTransformationHelper.cs
+++ src/MicroService.Service/Helpers/GeoTransformationHelper.cs
@@ -90,94 +90,16 @@ namespace MicroService.Service.Helpers
             return new[] { result[1], result[0] };
         }
 
-        public static Geometry TransformGeometryToWgs84(Geometry geometry)
+        public static Geometry TransformGeometry(Geometry geometry, MicroService.Service.Models.Enum.Datum fromDatum, MicroService.Service.Models.Enum.Datum toDatum)
         {
-            if (geometry is Point point)
+            bool wgs84ToNad83 = fromDatum == Models.Enum.Datum.Wgs84 && toDatum == Models.Enum.Datum.Nad83;
+            bool nad83ToWgs84 = fromDatum == Models.Enum.Datum.Nad83 && toDatum == Models.Enum.Datum.Wgs84;
+
+            if (!wgs84ToNad83 && !nad83ToWgs84)
             {
-                var xy = new[] { point.X, point.Y };
-                xy = GeoTransformationHelper.ConvertNad83ToWgs84(xy);
-                point.X = xy[0];
-                point.Y = xy[1];
-            }
-            else if (geometry is LineString lineString)
-            {
-                for (int i = 0; i < lineString.Coordinates.Length; i++)
-                {
-                    var xy = new[] { lineString.Coordinates[i].X, lineString.Coordinates[i].Y };
-                    xy = GeoTransformationHelper.ConvertNad83ToWgs84(xy);
-                    lineString.Coordinates[i].X = xy[0];
-                    lineString.Coordinates[i].Y = xy[1];
-                }
-            }
-            else if (geometry is Polygon polygon)
-            {
-                for (int i = 0; i < polygon.Shell.Coordinates.Length; i++)
-                {
-                    var xy = new[] { polygon.Shell.Coordinates[i].X, polygon.Shell.Coordinates[i].Y };
-                    xy = GeoTransformationHelper.ConvertNad83ToWgs84(xy);
-                    polygon.Shell.Coordinates[i].X = xy[0];
-                    polygon.Shell.Coordinates[i].Y = xy[1];
-                }
-                for (int i = 0; i < polygon.Holes.Length; i++)
-                {
-                    for (int j = 0; j < polygon.Holes[i].Coordinates.Length; j++)
-                    {
-                        var xy = new[] { polygon.Holes[i].Coordinates[j].X, polygon.Holes[i].Coordinates[j].Y };
-                        xy = GeoTransformationHelper.ConvertNad83ToWgs84(xy);
-                        polygon.Holes[i].Coordinates[j].X = xy[0];
-                        polygon.Holes[i].Coordinates[j].Y = xy[1];
-                    }
-                }
+                throw new ArgumentException("Invalid datum combination.");
             }
 
-            return geometry;
-        }
-
-        public static Geometry TransformGeometryToNad83(Geometry geometry)
-        {
-            if (geometry is Point point)
-            {
-                var xy = new[] { point.X, point.Y };
-                xy = GeoTransformationHelper.ConvertWgs84ToNad83(xy);
-                point.X = xy[0];
-                point.Y = xy[1];
-            }
-            else if (geometry is LineString lineString)
-            {
-                for (int i = 0; i < lineString.Coordinates.Length; i++)
-                {
-                    var xy = new[] { lineString.Coordinates[i].X, lineString.Coordinates[i].Y };
-                    xy = GeoTransformationHelper.ConvertWgs84ToNad83(xy);
-                    lineString.Coordinates[i].X = xy[0];
-                    lineString.Coordinates[i].Y = xy[1];
-                }
-            }
-            else if (geometry is Polygon polygon)
-            {
-                for (int i = 0; i < polygon.Shell.Coordinates.Length; i++)
-                {
-                    var xy = new[] { polygon.Shell.Coordinates[i].X, polygon.Shell.Coordinates[i].Y };
-                    xy = GeoTransformationHelper.ConvertWgs84ToNad83(xy);
-                    polygon.Shell.Coordinates[i].X = xy[0];
-                    polygon.Shell.Coordinates[i].Y = xy[1];
-                }
-                for (int i = 0; i < polygon.Holes.Length; i++)
-                {
-                    for (int j = 0; j < polygon.Holes[i].Coordinates.Length; j++)
-                    {
-                        var xy = new[] { polygon.Holes[i].Coordinates[j].X, polygon.Holes[i].Coordinates[j].Y };
-                        xy = GeoTransformationHelper.ConvertWgs84ToNad83(xy);
-                        polygon.Holes[i].Coordinates[j].X = xy[0];
-                        polygon.Holes[i].Coordinates[j].Y = xy[1];
-                    }
-                }
-            }
-
-            return geometry;
-        }
-
-        public static Geometry TransformGeometry(Geometry geometry, bool wgs84ToNad83)
-        {
             if (geometry is Point point)
             {
                 var xy = new[] { point.X, point.Y };
@@ -218,8 +140,6 @@ namespace MicroService.Service.Helpers
 
             return geometry;
         }
-
-
 
 
     }
