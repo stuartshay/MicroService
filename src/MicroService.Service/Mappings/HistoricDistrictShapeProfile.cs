5ab5ba4201ca19bfd9b7b10f7292d7dd205aa49c
Stuart Shay <sshay@yahoo.com>
2023-03-14 13:18:24 -0400

    Update

--- src/MicroService.Service/Mappings/HistoricDistrictShapeProfile.cs
+++ src/MicroService.Service/Mappings/HistoricDistrictShapeProfile.cs
@@ -22,36 +22,18 @@ namespace MicroService.Service.Mappings
                     opt.MapFrom(src => string.IsNullOrEmpty(src.Attributes["caldate"].ToString()) ? null
                         : Regex.Replace(src.Attributes["caldate"].ToString(), @"\u0000", string.Empty)))
                 .ForMember(dest => dest.Current, opt => opt.MapFrom(src => src.Attributes["current_"].ToString()))
-
                 .ForMember(dest => dest.DesignationDate, opt =>
                     opt.MapFrom(src => string.IsNullOrEmpty(src.Attributes["desdate"].ToString()) ? null
                         : Regex.Replace(src.Attributes["desdate"].ToString(), @"\u0000", string.Empty)))
-
-
-
-
                 .ForMember(dest => dest.Extension, opt =>
                     opt.MapFrom(src => string.IsNullOrEmpty(src.Attributes["extension"].ToString()) ? null
                         : Regex.Replace(src.Attributes["extension"].ToString(), @"\u0000", string.Empty)))
-
-
-
                 .ForMember(dest => dest.LastAction, opt => opt.MapFrom(src => src.Attributes["last_actio"].ToString()))
-
-
                 .ForMember(dest => dest.OtherHearing, opt =>
                     opt.MapFrom(src => string.IsNullOrEmpty(src.Attributes["other_hear"].ToString()) ? null
                         : Regex.Replace(src.Attributes["other_hear"].ToString(), @"\u0000", string.Empty)))
-
-
-
-
                 .ForMember(dest => dest.PublicHearing, opt => opt.MapFrom(src => src.Attributes["public_hea"].ToString()))
                 .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Attributes["status_of_"].ToString()))
-
-
-
-
                 .ForMember(dest => dest.BoroCode, opt => opt.MapFrom(src => (int)Enum.Parse(typeof(Borough), src.Attributes["borough"].ToString())))
                 .ForMember(dest => dest.ShapeArea, opt => opt.MapFrom(src => double.Parse(src.Attributes["shape_area"].ToString())))
                 .ForMember(dest => dest.ShapeLength, opt => opt.MapFrom(src => double.Parse(src.Attributes["shape_leng"].ToString())))
