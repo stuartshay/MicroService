72f5c229de5eeb9348ba12007840907730061230
Stuart Shay <sshay@yahoo.com>
2023-03-14 13:03:26 -0400

    Update

--- src/MicroService.Service/Mappings/HistoricDistrictShapeProfile.cs
+++ src/MicroService.Service/Mappings/HistoricDistrictShapeProfile.cs
@@ -3,6 +3,7 @@ using MicroService.Service.Mappings.Base;
 using MicroService.Service.Models;
 using NetTopologySuite.Features;
 using System;
+using System.Text.RegularExpressions;
 
 namespace MicroService.Service.Mappings
 {
@@ -14,6 +15,24 @@ namespace MicroService.Service.Mappings
                 .ForMember(dest => dest.LPNumber, opt => opt.MapFrom(src => src.Attributes["lp_number"].ToString()))
                 .ForMember(dest => dest.AreaName, opt => opt.MapFrom(src => src.Attributes["area_name"].ToString()))
                 .ForMember(dest => dest.BoroName, opt => opt.MapFrom(src => src.Attributes["borough"].ToString()))
+                .ForMember(dest => dest.BoundaryName, opt =>
+                    opt.MapFrom(src => string.IsNullOrEmpty(src.Attributes["boundary_n"].ToString()) ? null
+                        : Regex.Replace(src.Attributes["boundary_n"].ToString(), @"\u0000", string.Empty)))
+                .ForMember(dest => dest.CalendarDate, opt => opt.MapFrom(src => src.Attributes["caldate"].ToString()))
+
+
+
+                //.ForMember(dest => dest.Current, opt => opt.MapFrom(src => src.Attributes["current"].ToString()))
+                //.ForMember(dest => dest.DesignationDate, opt => opt.MapFrom(src => src.Attributes["desdate"].ToString()))
+                //.ForMember(dest => dest.Extension, opt => opt.MapFrom(src => src.Attributes["extension"].ToString()))
+                //.ForMember(dest => dest.LastAction, opt => opt.MapFrom(src => src.Attributes["last_actio"].ToString()))
+                //.ForMember(dest => dest.OtherHearing, opt => opt.MapFrom(src => src.Attributes["other_hear"].ToString()))
+                //.ForMember(dest => dest.PublicHearing, opt => opt.MapFrom(src => src.Attributes["public_hea"].ToString()))
+                //.ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Attributes["status_of_"].ToString()))
+
+
+
+
                 .ForMember(dest => dest.BoroCode, opt => opt.MapFrom(src => (int)Enum.Parse(typeof(Borough), src.Attributes["borough"].ToString())))
                 .ForMember(dest => dest.ShapeArea, opt => opt.MapFrom(src => double.Parse(src.Attributes["shape_area"].ToString())))
                 .ForMember(dest => dest.ShapeLength, opt => opt.MapFrom(src => double.Parse(src.Attributes["shape_leng"].ToString())))
