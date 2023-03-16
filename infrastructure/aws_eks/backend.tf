177ae12a832b78ec7018d405dcaf6fa06cdf3f86
Stuart Shay <sshay@yahoo.com>
2023-03-16 05:17:02 -0400

    Cleanup Files

--- infrastructure/aws_eks/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    region = "us-east-2"
-    bucket = "tf-state-eks-cluster"
-    key = "eks-cluster.tfstate"
-  }
-}
--- infrastructure/ecs_terraform/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = 	"tf-state-fargate"
-    key    = 	"fargate-state.tfstate"
-    region = 	"us-east-2"
-  }
-}
