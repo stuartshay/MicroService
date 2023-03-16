509171d62a1dcbfd8a523e09352a60be8e618dc7
Stuart Shay <sshay@yahoo.com>
2023-03-16 05:17:02 -0400

    Cleanup Files

--- infrastructure/VPC_terraform/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = 	"tf-state-fargate"
-    key    = 	"vpc-fargate-state.tfstate"
-    region = 	"us-east-2"
-  }
-}
--- infrastructure/aws_eks/data.tf
+++
@@ -1,9 +0,0 @@
-# Get VPC ID
-data "terraform_remote_state" "infra" {
-  backend = "s3"
-  config  = {
-    bucket = "${var.vpc_state_bucket_name}"
-    key    = "vpc-fargate-state.tfstate"
-    region = "${var.region}"
-  }
-}
