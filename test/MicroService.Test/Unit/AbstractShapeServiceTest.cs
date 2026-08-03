bf7a072b12e8fa60498deb579c604ef5a6fa7c28
Stuart Shay <sshay@yahoo.com>
2026-08-02 23:05:56 -0400

    Add assertions to Sonar-reported tests (#469)
    * Add assertions to Sonar-reported tests
    
    * Strengthen Sonar test assertions

---
+++ test/MicroService.Test/Unit/AbstractShapeServiceTest.cs
@@ -0,0 +1,56 @@
+using AutoMapper;
+using MicroService.Service.Mappings;
+using MicroService.Service.Models;
+using MicroService.Service.Services.Base;
+using Microsoft.Extensions.Logging;
+using Xunit;
+
+namespace MicroService.Test.Unit
+{
+    public class AbstractShapeServiceTest
+    {
+        private readonly TestShapeService _service = new();
+
+        [Fact]
+        public void MatchAttributeValue_MatchesDoubleWithinTolerance()
+        {
+            const double value = 40.7128000005;
+
+            var result = _service.Match(value, 40.7128);
+
+            Assert.Equal(value, result);
+        }
+
+        [Fact]
+        public void MatchAttributeValue_RejectsDoubleOutsideTolerance()
+        {
+            var result = _service.Match(40.712800002, 40.7128);
+
+            Assert.Null(result);
+        }
+
+        [Theory]
+        [InlineData(42, 43)]
+        [InlineData(42, 43d)]
+        [InlineData(42d, 43)]
+        public void MatchAttributeValue_ReturnsNullForNumericMismatch(object value, object expectedValue)
+        {
+            var result = _service.Match(value, expectedValue);
+
+            Assert.Null(result);
+        }
+
+        private sealed class TestShapeService : AbstractShapeService<BoroughBoundaryShape, BoroughBoundaryShapeProfile>
+        {
+            public TestShapeService()
+                : base(Moq.Mock.Of<ILogger>(), Moq.Mock.Of<IMapper>())
+            {
+            }
+
+            public object? Match(object value, object expectedValue)
+            {
+                return MatchAttributeValue(value, expectedValue);
+            }
+        }
+    }
+}
--- test/MicroService.Test/Unit/ShapeFileTest.cs
+++ test/MicroService.Test/Unit/ShapeFileTest.cs
@@ -68,17 +68,19 @@ namespace MicroService.Test.Unit
                 features.Add(feature);
             }
 
-            var result = features;
-            foreach (var f in features)
+            Assert.NotEmpty(features);
+            Assert.All(features, feature =>
             {
-                var z = f.Geometry.Contains(new Point(1032999, 217570));
-                if (z)
-                {
-                    var z1 = f.Attributes["pct"];
-                    // var z2 = f.Attributes["patrol_bor"];
-                    // var z3 = f.Attributes["sector"];
-                }
-            }
+                Assert.NotNull(feature.Geometry);
+                Assert.NotNull(feature.Attributes);
+            });
+
+            var containingFeature = Assert.Single(features, feature =>
+                feature.Geometry.Contains(new Point(1032999, 217570)));
+            Assert.Contains("pct", containingFeature.Attributes.GetNames());
+            var precinct = containingFeature.Attributes["pct"];
+            Assert.NotNull(precinct);
+            Assert.NotEqual(DBNull.Value, precinct);
         }
 
         [Fact]
